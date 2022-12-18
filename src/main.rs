67da0df55ce642153c1ca0efb65bdb179c8e439a
Jared Chesebro <chesebrojared@gmail.com>
2022-12-17 18:03:00 -0800

    initial commit, outline

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use std::io;
+// C to F: F = C*(9/5) + 32
+// F to C: C = (F-32)*(5/9)
+fn main () {
+    println!("Do you want to convert to Celsius or Farenheit? Input C or F");
+    // empty string variable for user input
+
+    // io::std readline
+    // expect handling
+
+    // pass user variable into new variable
+
+    println!("You want to convert to" {})
+    println!("What temperature would you like to conver?");
+    // variable for temp
+
+    // io std read line
+    // expect
+
+    // convert temp into int include error hanlding
+
+    // match loop
+}
+
+// celsius to farenheit fn
+
+// farenheit to celsius fn
